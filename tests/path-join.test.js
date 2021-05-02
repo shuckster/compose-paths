59fcdd9972b60260c094b3d92d858e38e8bad77b
Conan Theobald <github@conans.co.uk>
2021-05-02 17:00:59 +0100

    Tests for shorthand example

--- tests/path-join.test.js
+++ tests/path-join.test.js
@@ -9,50 +9,80 @@ const {
   oneTabFor2Spaces
 } = require('./_common')
 
-test(`composePaths :: path.join / spaces`, () => {
-  process.env.RUNNING_JEST = 'true'
-  process.env.BUILD_FOR_NODE = 'true'
+describe('composePaths :: path.join', () => {
+  let composePaths
+  let zip
 
-  const join = require('path').join
-  const { composePaths, zip } = require('../src/compose-paths')
-  expect(join).toEqual(composePaths.__join)
+  beforeAll(() => {
+    process.env.RUNNING_JEST = 'true'
+    process.env.BUILD_FOR_NODE = 'true'
 
-  const paths = composePaths(PATHS)
-  expect(paths).toEqual(COMPOSED_PATHS)
-  expect(paths.aliases).toEqual(COMPOSED_PATHS_ALIASES)
+    const join = require('path').join
+    const imported = require('../src/compose-paths')
+    ;({ composePaths, zip } = imported)
 
-  const routes = composePaths(ROUTES)
-  expect(routes).toEqual(COMPOSED_ROUTES)
-  expect(routes.aliases).toEqual(COMPOSED_ROUTES_ALIASES)
-
-  const staticRoutes = zip(routes, paths, {
-    aliases: ['DEBUG_BOOKMARKLET', 'INDEX', 'HOME'],
-    ignoreAliases: true
+    expect(join).toEqual(composePaths.__join)
   })
 
-  expect(staticRoutes).toEqual(PATHS_AND_ROUTES_ZIPPED)
-})
+  test(`spaces`, () => {
+    const paths = composePaths(PATHS)
+    expect(paths).toEqual(COMPOSED_PATHS)
+    expect(paths.aliases).toEqual(COMPOSED_PATHS_ALIASES)
 
-test(`composePaths :: path.join / tabs`, () => {
-  process.env.RUNNING_JEST = 'true'
-  process.env.BUILD_FOR_NODE = 'true'
+    const routes = composePaths(ROUTES)
+    expect(routes).toEqual(COMPOSED_ROUTES)
+    expect(routes.aliases).toEqual(COMPOSED_ROUTES_ALIASES)
 
-  const join = require('path').join
-  const { composePaths, zip } = require('../src/compose-paths')
-  expect(join).toEqual(composePaths.__join)
+    const staticRoutes = zip(routes, paths, {
+      aliases: ['DEBUG_BOOKMARKLET', 'INDEX', 'HOME'],
+      ignoreAliases: true
+    })
 
-  const paths = composePaths(oneTabFor2Spaces(PATHS))
-  expect(paths).toEqual(COMPOSED_PATHS)
-  expect(paths.aliases).toEqual(COMPOSED_PATHS_ALIASES)
-
-  const routes = composePaths(oneTabFor2Spaces(ROUTES))
-  expect(routes).toEqual(COMPOSED_ROUTES)
-  expect(routes.aliases).toEqual(COMPOSED_ROUTES_ALIASES)
-
-  const staticRoutes = zip(routes, paths, {
-    aliases: ['DEBUG_BOOKMARKLET', 'INDEX', 'HOME'],
-    ignoreAliases: true
+    expect(staticRoutes).toEqual(PATHS_AND_ROUTES_ZIPPED)
   })
 
-  expect(staticRoutes).toEqual(PATHS_AND_ROUTES_ZIPPED)
+  test(`tabs`, () => {
+    const paths = composePaths(oneTabFor2Spaces(PATHS))
+    expect(paths).toEqual(COMPOSED_PATHS)
+    expect(paths.aliases).toEqual(COMPOSED_PATHS_ALIASES)
+
+    const routes = composePaths(oneTabFor2Spaces(ROUTES))
+    expect(routes).toEqual(COMPOSED_ROUTES)
+    expect(routes.aliases).toEqual(COMPOSED_ROUTES_ALIASES)
+
+    const staticRoutes = zip(routes, paths, {
+      aliases: ['DEBUG_BOOKMARKLET', 'INDEX', 'HOME'],
+      ignoreAliases: true
+    })
+
+    expect(staticRoutes).toEqual(PATHS_AND_ROUTES_ZIPPED)
+  })
+
+  test('shorthand', () => {
+    const pathFromRoute = composePaths(`
+
+      /src
+        /html
+          /pages
+            /index.html        = /
+            /about.html        = /about
+            /contact.html      = /contact
+
+    `)
+
+    expect(pathFromRoute).toEqual([
+      '/src',
+      '/src/html',
+      '/src/html/pages',
+      '/src/html/pages/index.html',
+      '/src/html/pages/about.html',
+      '/src/html/pages/contact.html'
+    ])
+
+    expect(pathFromRoute.aliases).toEqual(['/', '/about', '/contact'])
+
+    expect(pathFromRoute['/']).toEqual('/src/html/pages/index.html')
+    expect(pathFromRoute['/about']).toEqual('/src/html/pages/about.html')
+    expect(pathFromRoute['/contact']).toEqual('/src/html/pages/contact.html')
+  })
 })
