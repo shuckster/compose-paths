6a23a60f6eefffc2d965f5058e2f1b7eab9098ad
Conan Theobald <github@conans.co.uk>
2021-12-02 00:51:58 +0000

    Build: Drop makePromise(), pipeline refactor

--- build-config/esbuild.js
+++ build-config/esbuild.js
@@ -8,83 +8,83 @@ const pkg = require('../package.json')
 
 const { match, when, otherwise } = require('match-iz')
 const { paths, banner, outputs } = require('./common')
+const { pipe } = require('../src/fp')
 
-function main() {
-  Promise.all(
-    outputs.map(output =>
-      buildComposePaths({
-        ...output,
-        define: {
-          'process.env.RUNNING_JEST': '"false"',
-          'process.env.BUILD_FOR_NODE': `"${
-            output.format === 'cjs' ? 'true' : 'false'
-          }"`
-        }
-      })
-    )
+const main = () =>
+  pipe(
+    outputs,
+    $ => $.map(buildOutput),
+    $ => Promise.all($)
   )
-}
+
+const buildOutput = output =>
+  buildComposePaths({
+    ...output,
+    define: {
+      'process.env.RUNNING_JEST': '"false"',
+      'process.env.BUILD_FOR_NODE': `"${buildForNode(output.format)}"`
+    }
+  })
 
 function buildComposePaths({ file, format, define }) {
-  const decoder = new TextDecoder()
+  const buildOptions = {
+    entryPoints: [paths.SRC],
+    define,
+    format,
+    ...match(format)(
+      when('iife')({ platform: 'browser', globalName: 'composePaths' }),
+      otherwise({ platform: 'node' })
+    ),
+    target: ['es6'],
+    minify: true,
+    bundle: true,
+    write: false
+  }
   return esbuild
-    .build({
-      entryPoints: [paths.SRC],
-      define,
-      format,
-      ...match(format)(
-        when('iife')({ platform: 'browser', globalName: 'composePaths' }),
-        otherwise({ platform: 'node' })
-      ),
-      target: ['es6'],
-      minify: true,
-      bundle: true,
-      write: false
-    })
+    .build(buildOptions)
     .then(getConcatenatedEsbuildContent)
-    .then(buffer => decoder.decode(buffer))
+    .then($ => new TextDecoder().decode($))
     .then(text => banner(pkg, text))
     .then(writeTextFile(file))
 }
+
+const buildForNode = format => (format === 'cjs' ? 'true' : 'false')
 
 //
 // Helpers
 //
 
-function getConcatenatedEsbuildContent(build) {
-  return mergeTypedArrays(
-    build.outputFiles.map(out => out.contents),
-    Uint8Array
+const getConcatenatedEsbuildContent = build =>
+  pipe(
+    build.outputFiles,
+    $ => $.map(file => file.contents),
+    mergeTypedArrays(Uint8Array)
   )
-}
 
 // https://stackoverflow.com/a/56993335/127928
-const mergeTypedArrays = (arrays, type = Uint8Array) => {
-  const result = new type(arrays.reduce((acc, arr) => acc + arr.byteLength, 0))
-  let offset = 0
-  arrays.forEach(arr => {
-    result.set(arr, offset)
-    offset += arr.byteLength
-  })
-  return result
+const mergeTypedArrays = TypeConstructor => arrays =>
+  pipe(
+    arrays,
+    accumulatedByteLength,
+    byteLength => new TypeConstructor(byteLength),
+    concatArraysFrom(arrays)
+  )
+
+const concatArraysFrom = arrays => presizedArray => {
+  arrays.reduce((offset, arr) => {
+    presizedArray.set(arr, offset)
+    return offset + arr.byteLength
+  }, 0)
+  return presizedArray
+}
+
+function accumulatedByteLength(arrays) {
+  return arrays.reduce((acc, arr) => acc + arr.byteLength, 0)
 }
 
 function writeTextFile(path) {
-  return textContent => {
-    const [promise, resolve, reject] = makePromise()
-    fs.writeFile(path, textContent, err => (err ? reject(err) : resolve()))
-    return promise
-  }
-}
-
-function makePromise() {
-  let _resolve
-  let _reject
-  const promise = new Promise((resolve, reject) => {
-    _resolve = resolve
-    _reject = reject
-  })
-  return [promise, _resolve, _reject]
+  return textContent =>
+    Promise.resolve(path).then($ => fs.writeFileSync($, textContent))
 }
 
 //
